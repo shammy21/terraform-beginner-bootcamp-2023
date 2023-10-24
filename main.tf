36f7ab6d543dd4585a95db7835c6a476c84e57bc
shammy21 <eshamwell@outlook.com>
2023-10-23 22:10:31 -0400

    #13 migrated local state to terraform clouid using terraform cloud block (#14)

--- main.tf
+++ main.tf
@@ -1,15 +1,24 @@
 terraform {
+  cloud {
+    organization = "shammy21"
+
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
   required_providers {
     random = {
       source = "hashicorp/random"
       version = "3.5.1"
     }
-        aws = {
+
+  aws = {
       source = "hashicorp/aws"
       version = "5.22.0"
     }
   }
 }
+
 
 provider "aws" {
   # Configuration options
